4a7ad33f1cbe80b9de0ef5f4afa21811344ea262
Angela Crepaldi <angelacrepaldi@gmail.com>
2021-12-29 08:27:32 -0300

    Add CustomUser model and extend UserChangeForm and UserCreationForm.

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use CustomUser instead of the default User
+AUTH_USER_MODEL = 'users.CustomUser'
