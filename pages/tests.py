a5942940645f7dd6be9e406eaf021f5ea887ec1e
Angela Crepaldi <angelacrepaldi@gmail.com>
2021-12-30 19:30:15 -0300

    Add unit tests for About Page.

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -27,4 +27,30 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self) -> None:
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_about_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_about_page_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_about_page_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_about_page_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'I should not be on the page.')
+
+    def test_about_page_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
         )
