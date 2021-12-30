116857ecc8532ec69b3b7b227c4dccc3d63315c9
Angela Crepaldi <angelacrepaldi@gmail.com>
2021-12-30 16:01:57 -0300

    Add unit test for Sign Up functionality.

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import resolve, reverse
+
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 
 class CustomUserTests(TestCase):
@@ -7,6 +11,16 @@ class CustomUserTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.password = 'This154test'
+
+    def setUp(self) -> None:
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'I should not be on the page.')
 
     def test_create_user(self):
         User = get_user_model()
@@ -33,3 +47,28 @@ class CustomUserTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+
+    def setUp(self) -> None:
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'I should not be on the page.')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__
+        )
