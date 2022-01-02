b926125bb11969950d77f4a20cfea7f4758e201d
Angela Crepaldi <angelacrepaldi@gmail.com>
2022-01-02 18:42:42 -0300

    Add environment variables to docker-compose: SECRET_KEY and DEBUG.

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j3pj!g)%@civ2k%%3f2+di51=+=u0$8k=ak0*_(jvu*4jx@=ys'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['*']
 
